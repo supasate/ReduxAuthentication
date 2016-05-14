344f5b990de822b051fbca31f410070f26e897ca
Supasate Choochaisri <supasate.c@gmail.com>
2016-05-14 14:32:23 +0700

    Able to create user into database

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,11 +1,28 @@
+const User = require('../models/user');
+
 exports.signup = function(req, res, next) {
-  console.log(req.body);
+  const email = req.body.email;
+  const password = req.body.password;
+
   // See if a user with the given email exists
+  User.findOne({ email: email }, function(err, existingUser) {
+    if (err) { return next(err); }
 
-  // If a user with email does exist, return an error
+    // If a user with email does exist, return an error
+    if (existingUser) {
+      return res.status(422).send({ error: 'Email is in use' });
+    }
+    // If a user with email does NOT exist, create and save user record
+    const user = new User({
+      email: email,
+      password: password
+    });
 
-  // If a user with email does NOT exist, create and save user record
-
-  // Respond to request indicating the user was created
-  next();
+    user.save(function(err) {
+      if (err) { return next(err); }
+      
+      // Respond to request indicating the user was created
+      res.json(user);
+    });
+  });
 };
--- server/router.js
+++ server/router.js
@@ -1,9 +1,5 @@
 const authController = require('./controllers/authentication');
 
 module.exports = function(app) {
-  app.get('/', function(req, res, next) {
-    res.send(['waterbottle', 'phone', 'paper']);
-  });
-
   app.post('/signup', authController.signup);
 };
