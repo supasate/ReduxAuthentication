21e9d819d8c9986c986421adc4252ad5421ff288
Supasate Choochaisri <supasate.c@gmail.com>
2016-05-15 23:44:46 +0700

    redirect user to the feature route if login success

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { browserHistory } from 'react-router';
 
 const ROOT_URL = 'http://localhost:3090';
 
@@ -6,6 +7,14 @@ const signinUser = ({ email, password }) => {
   return function(dispatch) {
     // Submit email/password to the server
     axios.post(`${ROOT_URL}/signin`, { email, password })
+      .then(response => {
+
+        // redirect to the route '/feature'
+        browserHistory.push('/feature');
+      })
+      .catch(() => {
+
+      })
   };
 };
 
