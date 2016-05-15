3b660bb45c531d219eb20c36e1a2bf802cd4e470
Supasate Choochaisri <supasate.c@gmail.com>
2016-05-16 01:11:03 +0700

    Valdiate email, password, passwordConfirm fields

--- client/src/components/auth/signup.js
+++ client/src/components/auth/signup.js
@@ -15,6 +15,7 @@ class Signup extends Component {
         <fieldset className="form-group">
           <label>Email:</label>
           <input {...email} className="form-control" />
+          {email.touched && email.error && <div className="error">{email.error}</div>}
         </fieldset>
         <fieldset className="form-group">
           <label>Password:</label>
@@ -24,6 +25,7 @@ class Signup extends Component {
         <fieldset className="form-group">
           <label>Confirm Password:</label>
           <input {...passwordConfirm} type="password" className="form-control" />
+          {passwordConfirm.touched && passwordConfirm.error && <div className="error">{passwordConfirm.error}</div>}
         </fieldset>
         <button action="submit" className="btn btn-primary">Sign up!</button>
       </form>
@@ -33,6 +35,18 @@ class Signup extends Component {
 
 function validate(formProps) {
   const errors = {};
+
+  if (!formProps.email) {
+    errors.email = 'Please enter an email';
+  }
+
+  if (!formProps.password) {
+    errors.password = 'Please enter a password';
+  }
+
+  if (!formProps.passwordConfirm) {
+    errors.passwordConfirm = 'Please enter a password confirmation';
+  }
 
   if (formProps.password !== formProps.passwordConfirm) {
     errors.password = 'Passwords must match';
