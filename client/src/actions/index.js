d1acb387ef04e253781e6e3acfe529e7d90275ea
Supasate Choochaisri <supasate.c@gmail.com>
2016-05-15 23:52:09 +0700

    Update state to indicate user is authenticated

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { browserHistory } from 'react-router';
+import { AUTH_USER } from './types';
 
 const ROOT_URL = 'http://localhost:3090';
 
@@ -8,6 +9,8 @@ const signinUser = ({ email, password }) => {
     // Submit email/password to the server
     axios.post(`${ROOT_URL}/signin`, { email, password })
       .then(response => {
+        // Update state to indicate user is authenticated
+        dispatch({ type: AUTH_USER });
 
         // redirect to the route '/feature'
         browserHistory.push('/feature');
