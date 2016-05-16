812cb10b1b7cd282751bd0240ad0a2ce8f137a86
Supasate Choochaisri <supasate.c@gmail.com>
2016-05-16 09:15:33 +0700

    Add Welcome component to be in IndexRoute

---
+++ client/src/components/welcome.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Welcome extends Component {
+  render() {
+    return (
+      <div>
+        Welcome to our slice paradise
+      </div>
+    );
+  }
+}
+
+export default Welcome;
